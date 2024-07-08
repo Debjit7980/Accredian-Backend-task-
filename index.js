d8f23232e88480bb7873c835d5e137a3361967a5
DebjitSinghaRoy <debjitsingharoy007@gmail.com>
2024-07-08 16:40:17 +0530

    Updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 const prisma = new PrismaClient();
 
-const port = 8000;
+const port =5000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
@@ -26,54 +26,50 @@ app.get('/', (req, res) => {
 app.post("/referral", async(req, res) => {
     try {
         //res.status(200).json({ message: 'Referral received!' });
-        const user_name = req.body.userName;
-        const user_email = req.body.userEmail;
-        const course = req.body.course;
-        const ref_name = req.body.refName;
-        const ref_email = req.body.refEmail;
-        console.log(course);
+        const {user_name, user_email, course, ref_name, ref_email} = req.body;
+        console.log(course, user_name, user_email);
 
-        if (!user_name || !user_email || !course || !ref_name || !ref_email) {
-            return res.status(400).json({ error: 'All fields are required' });
-        }
+        // if (!user_name || !user_email || !course || !ref_name || !ref_email) {
+        //     return res.status(400).json({ error: 'All fields are required' });
+        // }
 
 
-        const referral = await prisma.referral.create({
-            data: {
-                user_name,
-                user_email,
-                course,
-                ref_name,
-                ref_email
-            }
-        });
+        // const referral = await prisma.referral.create({
+        //     data: {
+        //         user_name,
+        //         user_email,
+        //         course,
+        //         ref_name,
+        //         ref_email
+        //     }
+        // });
 
-        //Sending referral email
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.GMAIL_USER,
-                pass: process.env.GMAIL_PASS
-            }
-        });
+        // //Sending referral email
+        // const transporter = nodemailer.createTransport({
+        //     service: 'gmail',
+        //     auth: {
+        //         user: process.env.GMAIL_USER,
+        //         pass: process.env.GMAIL_PASS
+        //     }
+        // });
 
-        const mailOptions = {
-            from: process.env.GMAIL_USER,
-            to: ref_email,
-            subject: 'Course Referral',
-            text: `Hi ${ref_name},\n\n${user_name} has referred you to the ${course} course.\n\nBest regards,\nYour Team`
-        };
+        // const mailOptions = {
+        //     from: process.env.GMAIL_USER,
+        //     to: ref_email,
+        //     subject: 'Course Referral',
+        //     text: `Hi ${ref_name},\n\n${user_name} has referred you to the ${course} course.\n\nBest regards,\nYour Team`
+        // };
 
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.log('Error sending email:', error);
-            } else {
-                console.log('Email sent:', info.response);
-            }
-        });
+        // transporter.sendMail(mailOptions, (error, info) => {
+        //     if (error) {
+        //         console.log('Error sending email:', error);
+        //     } else {
+        //         console.log('Email sent:', info.response);
+        //     }
+        // });
 
-        res.status(200).json(referral);               
-        //res.status(200).json({ message: 'Form data received successfully' });
+        //res.status(200).json(referral);               
+        res.status(200).json({ message: 'Form data received successfully' });
     } catch (error) {
         console.error('Error handling request:', error);
         res.status(500).json({ error: 'Internal Server Error' });
